b95ca175fb300ed6d9b16d44fff47b5a53e33f05
naopeke <naopeke@protonmail.com>
2025-04-29 16:34:42 +0200

    add_to_cart.ts

---
+++ src/pages/base.page.ts
@@ -0,0 +1,24 @@
+import { Browser, Page, chromium } from "@playwright/test";
+
+export class BasePage {
+    browser: Browser;
+    page: Page;
+
+    constructor(browser: Browser, page: Page){
+        this.browser= browser;
+        this.page = page;
+    }
+
+    static async create(){
+        const browser = await chromium.launch({
+            headless: false,
+            slowMo: 1000,
+            devtools: true
+        });
+
+        const page = await browser.newPage();
+        
+        return { browser, page }
+    }
+
+}
--- src/tests/step-definitions/add_to_cart.ts
+++ src/tests/step-definitions/add_to_cart.ts
@@ -1,20 +1,24 @@
 import { Given, When, Then } from "@cucumber/cucumber";
 import { Page, Browser, chromium, expect } from "@playwright/test";
+import { BasePage } from "pages/base.page";
 
 let browser: Browser;
 let page: Page;
 
 Given ('A web browser is at the inventory page', async function(){
-    browser = await chromium.launch({
-        headless: false,
-        slowMo: 1000,
-        devtools: true
-    });
+    // browser = await chromium.launch({
+    //     headless: false,
+    //     slowMo: 1000,
+    //     devtools: true
+    // });
 
-    page = await browser.newPage();
+    // page = await browser.newPage();
+    const result = await BasePage.create();
+    browser = result.browser;
+    page = result.page;
     await page.goto('https://www.saucedemo.com/inventory.html');
 })
 
-When ('A user clicks "Add to cart" button for the product {string}', async function(){
+When ('A user clicks "Add to cart" button for the product {string}', async function(itemName: string){
     
 })
