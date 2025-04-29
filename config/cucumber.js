f60dbf0671ac8dcdc8bda889453b39c5b8bd7071
naopeke <naopeke@protonmail.com>
2025-04-29 15:02:51 +0200

    first commit

---
+++ config/cucumber.js
@@ -0,0 +1,25 @@
+module.exports ={
+    default: {
+        paths: [
+            "src/tests/features"
+        ], 
+        dryRun: false,
+        format: [
+            "progress-bar",
+            "summary",
+            "json:reports/cucumber-report.json",
+            "html:reports/cucumber-report.html"
+        ],
+        formatOptions: {
+            colors: true,
+            snippetInterface: "async-await" //added
+        },
+        require: [
+            "src/tests/step-definitions/*.ts",
+            "src/tests/step-definitions/*.js",
+        ],
+        requireModule: [
+            "ts-node/register"
+        ]
+    }
+}
